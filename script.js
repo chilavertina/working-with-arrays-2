1a6316a864c8449f61f6bf6e359f7edd99f87b9b
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-10-14 22:13:35 +0200

    povezivanje polja za transfer novca sa podacima korisnika

--- script.js
+++ script.js
@@ -154,6 +154,14 @@ btnLogin.addEventListener('click', function (e) {
   }
 });
 
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+});
+
 /* moje resenje prikaza bilansa
 const calcPrintBalance = function (vrednosti) {
   labelBalance.innerHTML = '';
